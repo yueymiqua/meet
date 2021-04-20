c80f57288e04d3d5fd7946939e2a60dabba71a2f
yueymiqua <pierre1116@gmail.com>
2021-04-19 22:31:51 -0400

    commented out running headless in end-to-end testing

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -5,9 +5,9 @@ describe('show/hide an event detail', () => {
   jest.setTimeout(30000); // by default the run time is 5000ms(5 secs)
   beforeAll(async () => {
     browser = await puppeteer.launch({
-    headless: false, // turn off headless mode which was by default set to true
-    slowMo: 250, // slow down by 250ms
-    ignoreDefaultArgs: ['--disable-extensions'] // ignores default setting that causes timeout errors
+    // headless: false, // turn off headless mode which was by default set to true
+    // slowMo: 250, // slow down by 250ms
+    // ignoreDefaultArgs: ['--disable-extensions'] // ignores default setting that causes timeout errors
   });
     page = await browser.newPage();
     await page.goto('http://localhost:3000/');
@@ -29,5 +29,5 @@ describe('show/hide an event detail', () => {
     await page.click('.event .details-btn');
     const eventDetails = await page.$('.event .event__Details');
     expect(eventDetails).toBeNull();
-  })
+  });
 });
