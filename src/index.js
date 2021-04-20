9094a295f5a6335ada7cc95cd60ae70ddaf6fd10
yueymiqua <pierre1116@gmail.com>
2021-04-20 13:13:27 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import * as atatus from 'atatus-js';
+import * as atatus from 'atatus-spa';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
