530a0fb73709a4b441345ab55c58a92f609563f7
yueymiqua <pierre1116@gmail.com>
2021-04-21 12:03:35 -0400

    implemented OOP for displaying info/error alerts in app

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import { InfoAlert } from './alert';
 
 class CitySearch extends Component {
   state = {
     query: '',
     suggestions: [],
     showSuggestions: undefined,
+    infoText: '',
   }
 
   handleInputChanged = (event) => {
@@ -12,8 +14,19 @@ class CitySearch extends Component {
     const suggestions = this.props.locations.filter((location) => {
       return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
     });
-    this.setState({ query: value, suggestions, })
-  }
+    if(suggestions.length === 0){
+      this.setState({
+        query: value,
+        infoText: "We cannot find the city you are looking for. Please try another city",
+      });
+    } else {
+      return this.setState({ 
+        query: value, 
+        suggestions,
+        infoText:"",
+      });
+    }
+  };
 
   handleItemClicked = (suggestion) => {
     this.setState({
@@ -27,6 +40,7 @@ class CitySearch extends Component {
   render() {
     return (
       <div className="CitySearch">
+        <InfoAlert text={this.state.infoText} />
         <input 
           type="text" 
           className="city" 
--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,22 +1,33 @@
 import React, { Component } from 'react';
+import { ErrorAlert } from './alert';
 
 class NumberOfEvents extends Component {
   state = {
     query: 32, // by default if no number is inputted, there will be maximum of 32 events
+    infoText:'',
   }
 
   updateNumberOfEvents = (event) => {
     const value = event.target.value
     this.props.updateEvents(null, value);
-    this.setState({
-      query: value
-    })
+    if(value < 1 || value > 32) {
+      this.setState({
+        query: value,
+        infoText: "Please enter number between 1 and 32",
+      })
+    } else {
+      return this.setState({
+        query: value,
+        infoText: '',
+      })
+    }
   }
 
   render() {
     return <div className="numberOfEvents">
-    <label>Number Of Events</label>
-    <input type="number" className="eventNumberInput" value={this.state.query} onChange={this.updateNumberOfEvents}/>
+      <label>Number Of Events</label>
+      <input type="number" className="eventNumberInput" value={this.state.query} onChange={this.updateNumberOfEvents}/>
+      <ErrorAlert className="ErrorAlert" text={this.state.infoText}/>
     </div>
   }
 }
